482fa681bff01b91ef159e0c9bd2af8252a92ccc
Michał Łaszczewski <michal@cloudforge.pl>
2016-03-11 05:27:44 +0100

    reactive api

--- mix.exs
+++ mix.exs
@@ -31,7 +31,9 @@ defmodule Reactive.Api.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:reactive_entity, github: "ReactiveWeb/reactive_entity"},
-     { :jsx,  "~> 2.6.2" }]
+    [
+     {:reactive_entity, git: "git@bitbucket.org:ScalableEngineering/reactive-entity.git"},
+     { :jsx,  "~> 2.6.2" }
+     ]
   end
 end
